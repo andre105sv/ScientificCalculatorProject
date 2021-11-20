8f900f51df826b92df9a67360366176d2fe4082c
Filippo Somma <fil.somma@gmail.com>
2021-11-20 15:16:35 +0100

    FilippoBranch Added

--- ScientificCalculator/src/scientificcalculator/ScientificCalculator.java
+++ ScientificCalculator/src/scientificcalculator/ScientificCalculator.java
@@ -20,7 +20,7 @@ public class ScientificCalculator {
        System.out.println("Sono Luca");
        System.out.println("Sono Antonio");
        System.out.println("Sono Filippo");
-        
+       System.out.println("Branch Filippo");
     }
     
 }
