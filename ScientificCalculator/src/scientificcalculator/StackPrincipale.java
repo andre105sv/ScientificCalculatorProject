08aaf4153f979044aa2136cbe3cf3a364e8ad1ca
anton <anton@VivoBookDiAntonio.mshome.net>
2021-11-23 10:56:54 +0100

    Collegamento IDE-GitHub Antonio

---
+++ ScientificCalculator/src/scientificcalculator/StackPrincipale.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package scientificcalculator;
+import java.util.*;
+/**
+ *
+ * @author anton
+ */
+public class StackPrincipale {
+    private List<ComplexNumber> stack;
+    private int size;
+    
+//Crea l'oggetto StackPrincipale inizializzando i suoi attributi. Lo stack è implementato con una LinkedList
+    public StackPrincipale(){
+        this.stack = new LinkedList<>();
+        this.size = this.stack.size();
+    }
+
+//Inserisce un elemento in coda alla LiskedList, in modo analogo a ciò che avverrebbe in uno stack    
+    public void insertNumber(ComplexNumber number){
+        this.stack.add(number);
+        this.size=this.stack.size();
+    }
+    
+//restituisce il numero di elementi nello stack
+    public int getSize(){
+        return this.size;
+    }
+    
+    public ComplexNumber removeLastNumber(){
+        ComplexNumber number = this.stack.remove(this.stack.size()-1);
+        this.size=this.stack.size();
+        return number;
+    }
+    
+
+    
+
+}
---
+++ ScientificCalculator/test/scientificcalculator/StackPrincipaleTest.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scientificcalculator;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author anton
+ */
+public class StackPrincipaleTest {
+    
+    public StackPrincipaleTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of insertNumber method, of class StackPrincipale.
+     */
+    @Test
+    public void testInsertNumber() {
+        System.out.println("insertNumber");
+        ComplexNumber number = new ComplexNumber(4,5);
+        StackPrincipale instance = new StackPrincipale();
+        int prevSize = instance.getSize();
+        instance.insertNumber(number);
+        assertEquals(prevSize+1,instance.getSize());
+        
+        
+    }
+
+    /**
+     * Test of getSize method, of class StackPrincipale.
+     */
+    @Test
+    public void testGetSize() {
+        System.out.println("getSize");
+        StackPrincipale instance = new StackPrincipale();
+        int expResult = 1;
+        instance.insertNumber(new ComplexNumber(5,5));
+        int result = instance.getSize();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of removeLastNumber method, of class StackPrincipale.
+     */
+    @Test
+    public void testRemoveLastNumber() {
+        System.out.println("removeLastNumber");
+        StackPrincipale instance = new StackPrincipale();
+        ComplexNumber expResult = new ComplexNumber(5,5);
+        instance.insertNumber(expResult);
+        ComplexNumber result = instance.removeLastNumber();
+        assertEquals(expResult, result);
+        
+    }
+    
+}
