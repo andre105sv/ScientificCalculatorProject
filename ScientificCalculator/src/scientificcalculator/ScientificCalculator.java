3ae0fa9224cbe0e11df5e689214fc5beb7e3ed36
andre105sv <a.landi88@studenti.unisa.it>
2021-11-23 14:37:14 +0100

    v1.1 - classe ComplexNumber aggiornata

--- ScientificCalculator/src/scientificcalculator/ScientificCalculator.java
+++ ScientificCalculator/src/scientificcalculator/ScientificCalculator.java
@@ -22,10 +22,35 @@ public class ScientificCalculator {
      */
     public static void main(String[] args) {
         
-       System.out.println("Progetto: ScientificCalculator ");
-       System.out.println("Versione: 1.0");
-       
+        System.out.println("Progetto: ScientificCalculator ");
+        System.out.println("Versione: 1.1");
+
+        //+ 42 - 35j oppure 42 - 35j oppure - 42 - 35j oppure + 43 oppure - 34 oppure 56
         
+        String s1 = "+ 42 - 35j";
+        ComplexNumber c1 = new ComplexNumber(s1);
+        System.out.println(c1.toString());
+        
+        String s2 = "42 - 35j";
+        ComplexNumber c2 = new ComplexNumber(s2);
+        System.out.println(c2.toString());
+        
+        String s3 = "- 42 - 35j";
+        ComplexNumber c3 = new ComplexNumber(s3);
+        System.out.println(c3.toString());
+        
+        String s4 = "+ 43";
+        ComplexNumber c4 = new ComplexNumber(s4);
+        System.out.println(c4.toString());
+        
+        String s5 = "- 34";
+        ComplexNumber c5 = new ComplexNumber(s5);
+        System.out.println(c5.toString());
+
+        String s6 = "56";
+        ComplexNumber c6 = new ComplexNumber(s6);
+        System.out.println(c6.toString());
+
     }
-    
+
 }
---
+++ ScientificCalculator/test/scientificcalculator/ComplexNumberTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package scientificcalculator;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Andrea
+ */
+public class ComplexNumberTest {
+
+    private ComplexNumber c;
+
+    @Before
+    public void setUp(){
+        c = new ComplexNumber();
+    }
+
+}
