68b6cd70ce6283c1baa833b6c9533d3ebc289c34
Filippo Somma <fil.somma@gmail.com>
2021-11-23 08:53:10 +0100

    fix n.1 dovuto a sintassi non rispettata.

--- ScientificCalculator/src/scientificcalculator/ComplexNumber.java
+++ ScientificCalculator/src/scientificcalculator/ComplexNumber.java
@@ -14,17 +14,17 @@ public class ComplexNumber {
     private double immPart;
 
     public ComplexNumber(String s) {
-        if (s.contains("+")){
-            String tmp[] = s.split("+");
+        if (s.contains("\\+")){
+            String tmp[] = s.split("\\+");
             this.realPart = Double.parseDouble(tmp[0]);
             this.immPart = Double.parseDouble(tmp[1]);
         }
-        if (s.contains("j")&&(!(s.contains("+")))){
+        if (s.contains("j")&&(!(s.contains("\\+")))){
             this.realPart=0;
             this.immPart=Double.parseDouble(s.replace("j",""));
             
         }
-        if (!(s.contains("j"))&&(!(s.contains("+")))){
+        if (!(s.contains("j"))&&(!(s.contains("\\+")))){
             this.realPart=Double.parseDouble(s);
             this.immPart=0;
         }
@@ -46,7 +46,10 @@ public class ComplexNumber {
 
     @Override
     public String toString() {
-        return "ComplexNumber{" + "realPart=" + realPart + ", immPart=j" + immPart + '}';
+        if (immPart<0)
+            return "("+realPart + "-" + immPart + "j)";
+        else
+            return "("+realPart + "+" + immPart + "j)";
     }
     
     
